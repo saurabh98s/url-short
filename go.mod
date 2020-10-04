e6e16a079c188ba535d273c1cd4604cd5c26b16e
Saurabh suman <geek.saurabh98@gmail.com>
2020-10-04 17:19:19 +0530

    add:redis layer

--- go.mod
+++ go.mod
@@ -4,11 +4,12 @@ go 1.13
 
 require (
 	github.com/go-errors/errors v1.1.1
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/pkg/errors v0.9.1
 	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
 	github.com/vmihailenco/msgpack v4.0.4+incompatible
 	go.mongodb.org/mongo-driver v1.4.1
-	golang.org/x/tools v0.0.0-20201002184944-ecd9fd270d5d // indirect
+	golang.org/x/tools v0.0.0-20201002184944-ecd9fd270d5d
 	google.golang.org/appengine v1.6.6 // indirect
 	gopkg.in/dealancer/validate.v2 v2.1.0
 )
--- repository/mongo/mongo.go
+++ repository/mongo/mongo.go
@@ -6,6 +6,7 @@ import (
 	"url-short/shortener"
 
 	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -17,33 +18,63 @@ type mongoRepository struct {
 	timeout  time.Duration
 }
 
-
-func newMongoClient(mongoURL string,mongoTimeout int) (*mongo.Client,error) {
-	ctx,cancel:=context.WithTimeout(context.Background(),time.Duration(mongoTimeout)*time.Second)
+func newMongoClient(mongoURL string, mongoTimeout int) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(mongoTimeout)*time.Second)
 	defer cancel()
-	client,err:= mongo.Connect(ctx,options.Client().ApplyURI(mongoURL))
-	if err!=nil {
-		return nil,err
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		return nil, err
 	}
-	err=client.Ping(ctx,readpref.Primary())
-	if err!=nil {
-		return nil,err
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
 	}
-	return client,nil
+	return client, nil
 }
 
-func NewMongoRepository(mongoURL,mongoDB string, mongoTimeout int)(shortener.RedirectRepository,error){
-	repo:= &mongoRepository{
-		timeout: time.Duration(mongoTimeout)*time.Second,
+func NewMongoRepository(mongoURL, mongoDB string, mongoTimeout int) (shortener.RedirectRepository, error) {
+	repo := &mongoRepository{
+		timeout:  time.Duration(mongoTimeout) * time.Second,
 		database: mongoDB,
 	}
-	client,err:=newMongoClient(mongoURL,mongoTimeout)
+	client, err := newMongoClient(mongoURL, mongoTimeout)
 	if err != nil {
-		return nil,errors.Wrap(err,"[ERROR] at repository.NewMongoRepository")
+		return nil, errors.Wrap(err, "[ERROR] at repository.NewMongoRepository")
 	}
-	repo.client=client
-	return repo,nil //wont work unless you attach the interface aka the two methods required by the interface.
+	repo.client = client
+	return repo, nil //wont work unless you attach the interface aka the two methods required by the interface.
 }
 
+func (r *mongoRepository) Find(code string) (*shortener.Redirect, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), r.timeout)
+	defer cancel()
+	redirect := &shortener.Redirect{}
+	collection := r.client.Database(r.database).Collection("redirects")
+	filter := bson.M{"code": code}
+	err := collection.FindOne(ctx, filter).Decode(&redirect)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, errors.Wrap(shortener.ErrRedirectNotFound, "[ERROR] repository.Redirect.Find")
+		}
+		return nil, errors.Wrap(err, "[ERROR] repository.Redirect.Find")
+	}
+	return redirect,nil
+}
 
-
+func (r *mongoRepository) Store(redirect *shortener.Redirect) error {
+	ctx, cancel := context.WithTimeout(context.Background(), r.timeout)
+	defer cancel()
+	collection := r.client.Database(r.database).Collection("redirects")
+	_, err := collection.InsertOne(
+		ctx,
+		bson.M{
+			"code":       redirect.Code,
+			"url":        redirect.URL,
+			"created_at": redirect.CreatedAt,
+		},
+	)
+	if err != nil {
+		return errors.Wrap(err, "[ERROR] repository.Redirect.Store")
+	}
+	return nil
+}
