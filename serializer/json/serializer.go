017d4464b1273045f158a2bd0f4beff0d0984aa3
Saurabh suman <geek.saurabh98@gmail.com>
2020-10-04 15:33:04 +0530

    add:serializer support for json and msgpack

---
+++ serializer/json/serializer.go
@@ -0,0 +1,28 @@
+package json
+
+import (
+	"encoding/json"
+	"url-short/shortener"
+
+	"github.com/pkg/errors"
+)
+
+type Redirect struct{}
+
+//Decode returns a Redirect type data 
+func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
+	redirect := &shortener.Redirect{}
+	if err := json.Unmarshal(input, redirect); err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Decode")
+	}
+	return redirect, nil
+}
+
+// Encode returns a array of bytes
+func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
+	rawMsg, err := json.Marshal(input)
+	if err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Encode")
+	}
+	return rawMsg, nil
+}
